a34f4229776c9179944d2515c4152f2095d43a55
Joe Webster <joe@makegofast.com>
2021-12-10 12:40:32 -0500

    Day 9 part 1 re-refactor to make part 2 doable

--- 9/__init__.py
+++ 9/__init__.py
@@ -1,6 +1,29 @@
 class AutoNavigation(object):
     def __init__(self):
-        pass
+        self.basins = []
+        self.visited = {}
+
+    def in_basin(self, row, col):
+        for basin in self.basins:
+            for point in basin:
+                if point[0] == row and point[1] == col:
+                    return True
+
+    def mark_visited(self, row, col):
+        self.visited.setdefault(row, {})
+        self.visited[row][col] = True
+
+    def is_visited(self, row, col):
+        try:
+            return self.visited[row][col]
+        except:
+            return None
+
+    def value_at(self, row, col):
+        if row < 0 or col < 0 or row > self.height or col > self.width: 
+            return None
+
+        return self.data[row][col]
 
     def load_data(self, data):
         self.data = []
@@ -15,37 +38,36 @@ class AutoNavigation(object):
 
         self.height = len(self.data)-1
 
-    def value_at(self, row, col):
-        if row < 0 or col < 0 or row > self.height or col > self.width: 
-            return None
-
-        return self.data[row][col]
-
-    def detect_basins(self):
-        basins = []
+    def detect_basins(self, algorithm):
+        algo_fn = getattr(self, algorithm)
 
         for row in range(0, self.height+1):
             for col in range(0, self.width+1):
-                basin, visited = self.crawl(row, col)
-                if basin:
-                    basins.append(basin)
+                algo_fn(row, col)
 
-        print(self.status(basins))
-        return basins
+        print(self.status())
 
-    def crawl(self, row, col):
-        visited = {}
+    def status(self):
+        ret = []
+
+        print('Status')
+        for row in range(0, self.height+1):
+            line = ''
+            for col in range(0, self.width+1):
+                val = self.value_at(row, col)
+                if self.in_basin(row, col):
+                    val = bcolor('red', val)
+                elif val == 9:
+                    val = bcolor('green', val)
+                elif self.is_visited(row, col):
+                    val = bcolor('blue', val)
+                line += str(val)
+            print(line)
+
+    def crawl_part_1(self, row, col):
         basin = []
 
-        visited.setdefault(row, {})
-        visited[row].setdefault(col, False)
-
-        visited[row][col] = True
-
-        my_val = self.value_at(row, col)
-
-        if my_val == 9:
-            return None, visited
+        self.mark_visited(row, col)
 
         scan_points = [
             [row-1, col],
@@ -54,57 +76,40 @@ class AutoNavigation(object):
             [row, col+1]
         ]
 
-        for point in scan_points:
-            point_val = self.value_at(point[0], point[1])
-            if point_val == None: #Out of bounds
+        origin_val = self.value_at(row, col)
+
+        if origin_val == 9:
+            return None
+
+        for target in scan_points:
+            target_val = self.value_at(target[0], target[1])
+
+            if target_val == None: #Out of bounds
                 continue
 
-            if point_val < my_val:
-                return None, visited
+            if target_val < origin_val:
+                return None
 
-        basin.append([row,col,my_val])
+        self.basins.append([[row, col, origin_val]])
 
-        return basin, visited
+    def crawl_part_2(self, row, col):
+        pass
 
     def calculate_risk_level(self, basins):
         risk = 0
 
-        for basin in basins:
+        for basin in self.basins:
             for p in basin:
                 risk += 1 + p[2]
     
         return risk 
 
-    def in_basin(self, basins, row, col):
-        for basin in basins:
-            for p in basin:
-                if p[0] == row and p[1] == col:
-                    return True
-
-    def status(self, basins=[], visited={}):
-        ret = []
-
-        print('Status')
-        for row in range(0, self.height):
-            line = '' 
-
-            for col in range(0, self.width):
-                val = self.value_at(row, col)
-                if self.in_basin(basins, row, col):
-                    val = bcolor('red', val)
-                elif visited.get(row, {}).get(col, False):
-                    val = bcolor('blue', val)
-                line += str(val)
-
-            ret.append(line)
-
-        ret.append('')
-        return '\n'.join(ret)
 
 def bcolor(color, text):
     colors = {
         'blue': '\033[94m',
         'red': '\033[91m',
+        'green': '\033[92m',
         'reset': '\033[0m'
     }
 
@@ -116,18 +121,27 @@ def get_data():
 def get_test_data():
     return [l.strip() for l in open('test_data.txt', 'r').readlines()]
 
-def solution(data):
+def solution_part_1(data):
     an = AutoNavigation()
 
     an.load_data(data)
-    lp = an.detect_basins()
+    lp = an.detect_basins('crawl_part_1')
+
+    rl = an.calculate_risk_level(lp)
+    print('Risk Level:',rl)
+
+def solution_part_2(data):
+    an = AutoNavigation()
+
+    an.load_data(data)
+    lp = an.detect_basins('crawl_part_2')
 
     rl = an.calculate_risk_level(lp)
     print('Risk Level:',rl)
 
 if __name__ == '__main__':
     data = get_test_data()
-    solution(data)
+    solution_part_1(data)
 
     data = get_data()
-    solution(data)
+    solution_part_1(data)
