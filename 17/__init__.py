371c5d70264e60946865d7ed7c53c40223c15a99
Joe Webster <joe@makegofast.com>
2021-12-21 19:47:59 -0500

    Day 17 part 2 (and a fix for part 1). A ⭐️ is a ⭐️

--- 17/__init__.py
+++ 17/__init__.py
@@ -6,12 +6,15 @@ import lib.text
 
 class ProbeTargetingSystem(object):
     def __init__(self):
+        self.reset()
+
+    def reset(self):
         self.x_pos = 0
         self.y_pos = 0
         self.x_vel_init = self.x_vel = 0
         self.y_vel_init = self.y_vel = 0
-
         self.track = []
+        self.hit = self.miss = self.warpshot = self.overshot = self.undershot = None
         self.add_track_point(self.x_pos, self.y_pos)
 
     def add_track_point(self, x, y):
@@ -23,36 +26,28 @@ class ProbeTargetingSystem(object):
     def highest_track(self):
         return max([p[1] for p in self.track])
 
-    def over_shot(self):
-        if self.track[-1][0] > self.target['x2']:
-            return True
-
     def in_target(self, x, y):
         x_check = self.target['x1'] <= x <= self.target['x2']
         y_check = self.target['y1'] <= y <= self.target['y2']
 
         return True if x_check and y_check else False
 
-    def is_hit(self):
+    def detect_hit(self):
         for point in self.track:
             if self.in_target(point[0], point[1]):
                 return point
 
     def fire(self, x_vel, y_vel):
-        self.track = []
-        self.x_pos = 0
-        self.y_pos = 0
-        self.x_vel_init = self.x_vel = x_vel
-        self.y_vel_init = self.y_vel = y_vel
+        self.reset()
 
-        self.add_track_point(self.x_pos, self.y_pos)
+        self.x_vel = self.x_vel_init = x_vel
+        self.y_vel = self.y_vel_init = y_vel
 
-        while self.x_pos < self.target['x2'] and self.y_pos > self.target['y2']:
+        while not self.hit and not self.miss: #self.x_pos < self.target['x2'] and self.y_pos > self.target['y2']:
             self.step()
 
-        if self.is_hit():
-            #self.status()
-            return self.is_hit()
+        if self.hit:
+            return self.detect_hit()
 
     def load_data(self, data):
         p = re.compile(r'target area: x=(?P<x1>[0-9-]+)..(?P<x2>[0-9-]+), y=(?P<y1>[0-9-]+)..(?P<y2>[0-9-]+)')
@@ -66,6 +61,20 @@ class ProbeTargetingSystem(object):
 
         self.add_track_point(self.x_pos, self.y_pos)
 
+        if self.in_target(self.x_pos, self.y_pos):
+            self.hit = True
+            self.miss = self.warpshot = self.overshot = self.undershot = False
+
+        elif self.y_pos < self.target['y1']:
+            self.hit = False
+            self.miss = True
+            if self.x_pos < self.target['x1']:
+                self.undershot = True
+            elif self.x_pos > self.target['x2']:
+                self.overshot = True
+            else:
+                self.warpshot = True
+
         if self.x_vel != 0:
             self.x_vel += -1 if self.x_vel > 0 else 1
 
@@ -73,6 +82,7 @@ class ProbeTargetingSystem(object):
 
     def status(self):
         print("Initial:", self.x_vel_init, self.y_vel_init)
+        print("Hit=%s Miss=%s Warpshot=%s Undershot=%s Overshot=%s:" % (self.hit, self.miss, self.warpshot, self.undershot, self.overshot))
         print(self.track)
 
         left = 0
@@ -99,15 +109,30 @@ class ProbeTargetingSystem(object):
                 line += char
             print(line)
 
-        hit = self.is_hit()
+        hit = self.detect_hit()
         if hit:
             print('Hit:', hit)
         else:
             print('Miss :(')
 
+        print("Overshot", self.overshot)
         print('Highest X:', self.highest_track())
         print()
 
+
+def find_all_hits(pts):
+    hits = []
+
+    for x_vel in range(0, 1000):
+        for y_vel in range(-1000, 1000):
+            hit = pts.fire(x_vel, y_vel)
+
+            if hit:
+                hits.append([x_vel, y_vel]) 
+            elif pts.overshot:
+                break
+
+    return hits
 
 def find_coolest_shot(pts):
     x_vel = 1 
@@ -128,14 +153,14 @@ def find_coolest_shot(pts):
 
             y_vel += 1
         else:
-            over_shot = pts.over_shot()
-            if over_shot: 
+            overshot = pts.overshot
+            if overshot: 
                 y_vel += 1
                 x_vel -= 1
             else:
                 x_vel += 1
 
-        print(i, 'HIT' if hit else 'MISS', highest, 'xv', this_xv, 'yv', this_yv, hit, 'over shot' if over_shot else '')
+        print(i, 'HIT' if hit else 'MISS', highest, 'xv', this_xv, 'yv', this_yv, hit, 'over shot' if overshot else '')
 
     print("Highest Hit:", highest)
 
@@ -162,13 +187,17 @@ def test_part_1(data):
     pts.fire(0, 10)
 
 def solution_part_2(data):
-    pass
+    pts = ProbeTargetingSystem()
+    pts.load_data(data) 
+    hits = find_all_hits(pts)
+    print(hits)
+    print('Hits:', len(hits))
 
 if __name__ == '__main__':
     #test_part_1(get_test_data())
 
     #solution_part_1(get_test_data())
-    solution_part_1(get_data())
+    #solution_part_1(get_data())
 
     #solution_part_2(get_test_data())
-    #solution_part_2(get_data())
+    solution_part_2(get_data())
